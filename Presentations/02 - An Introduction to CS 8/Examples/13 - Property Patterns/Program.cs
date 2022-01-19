20b4896591bd4d995b84e8578cbfbcd0fb922d19
Jesper Gulmann Henriksen <jesper@gulmann.dk>
2022-01-19 16:53:53 +0100

    Minor project adjustments

--- Presentations/02 - An Introduction to CS 8/Examples/13 - Property Patterns/Program.cs
+++ Presentations/02 - An Introduction to CS 8/Examples/13 - Property Patterns/Program.cs
@@ -27,15 +27,6 @@ namespace Wincubate.CS8.Slide13
                 _ => $"Well done coding SOLID, {se.Level}... :-)"
             };
 
-        //static string? Evaluate(SoftwareEngineer? se) =>
-        //    se switch
-        //    {
-        //        { Level: Level.Lead } => $"{se.FullName} does great work",
-        //        { Level: Level.Chief } => $"You da boss, {se.FullName}",
-        //        null => "You're not even a software engineer, dude!",
-        //        _ => $"Well done coding SOLID, {se.Level}... :-)"
-        //    };
-
         static void HailTheKing(Employee employee)
         {
             if( employee is SoftwareEngineer { Level: Level.Chief, FullName: "Uncle Bob" })
